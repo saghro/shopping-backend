80bfd273da161a448c0631c51bbcbcda52256f89
saghro <ayoubsaghro27@gmail.com>
2023-11-25 23:47:37 +0100

    add im

--- src/main/java/org/sid/lightecom/dao/ProductRepository.java
+++ src/main/java/org/sid/lightecom/dao/ProductRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product ,Long> {
   @RestResource(path = "/selectedProducts")
